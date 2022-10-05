32d51fde64c11a6b224c98898862956aa34f4973
Chris Lovett <clovett@microsoft.com>
2022-10-04 21:27:23 -0700

    fix includes.

--- NaturalNumberGame/AdditionWorld.lean
+++ NaturalNumberGame/AdditionWorld.lean
@@ -2,3 +2,5 @@ import AdditionWorld.Level1
 import AdditionWorld.Level2
 import AdditionWorld.Level3
 import AdditionWorld.Level4
+import AdditionWorld.Level5
+-- import AdditionWorld.Level6 -- bugbug
